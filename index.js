c4e38bd9d13207343ec38a957129c6b617c0c9ba
skhan75 <sami.ahmadkhan12@gmail.com>
2022-10-10 23:46:18 -0700

    Added more watchlists functionalities, restructured project

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
-const TDClient = require("./client")
+const TDClient = require("./lib/client")
 
 class TDAmeritrade extends TDClient {
     constructor(config) {
         super(config);
     }
 }
-// TDAmeritrade.prototype.authorize = authorize
-// TDAmeritrade.prototype.login = login
-// TDAmeritrade.prototype.on = on
-
-// TDAmeritrade.prototype.account = function (accountId) {
-//     const instance = new TDAccount(accountId);
-//     instance.request = this.request;
-//     return instance;
-// };
-
-// TDAmeritrade.prototype.market = function () {
-//     const instance = new TDMarketData();
-//     instance.request = this.request;
-//     return instance;
-// };
 
 module.exports = {
     TDAmeritrade
